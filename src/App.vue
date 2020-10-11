ddcc1635e9754104395c8fe1dab427c6d7cb09c6
RAPJThompson <allip01@hotmail.com>
2020-10-11 12:38:35 -0600

    finish up

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,55 @@
 <template>
 	<div id="app" class="map-container">
 		<img id="myimage" src="../images/ArdosSectorMap.jpg" alt="Sector Map" usemap="#InfoLayer" class="map"/>
+    <div class="legend">
+      <div class="title">Legend</div>
+      <ul class="list">
+        <li><span class="legend-box red"></span> Shaun</li>
+        <li><span class="legend-box blue"></span> Mattie</li>
+        <li><span class="legend-box yellow"></span> Ravar</li>
+        <li><span class="legend-box green"></span> Robert</li>
+      </ul>
+    </div>
 		<svg class="overlay" width="100%" @click="displayZone=null">
-      <polygon shape="poly" class='overlay-zone' :class="zoneColour('orbitalStation')" 
-        @click.stop="handleClick('orbitalStation')"
+      <polygon shape="poly" class='overlay-zone' :class="zoneColour('orbitalStation')" @click.stop="handleClick('orbitalStation')"
 						:points="coords.orbitalStation" href="#OrbitalStation" alt="Some Text" />
-      <polygon shape="poly" class="overlay-zone" :class="zoneColour('commsRelay')" 
-        @click.stop="handleClick('commsRelay')"
+      <polygon shape="poly" class="overlay-zone" :class="zoneColour('commsRelay')" @click.stop="handleClick('commsRelay')"
 						:points="coords.commsRelay" href="#CommsRelay" alt="More Text" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('ancientRuins')" 
-        @click.stop="handleClick('ancientRuins')"
+      <text class="noselect" x="1160" y="750" text-anchor="middle" fill="white" font-size="30">Pelius</text>
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('ancientRuins')" @click.stop="handleClick('ancientRuins')"
 						:points="coords.ancientRuins" href="#AncientRuins" alt="Ancient Ruins" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('hiveCity')" 
-        @click.stop="handleClick('hiveCity')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('hiveCity')" @click.stop="handleClick('hiveCity')"
 						:points="coords.hiveCity" href="#Pelius" alt="Hive City of Pelius" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('commandComplex')" 
-        @click.stop="handleClick('commandComplex')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('commandComplex')" @click.stop="handleClick('commandComplex')"
 						:points="coords.commandComplex"	href="#CommandComplex" alt="Command Complex" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('wasteland')" 
-        @click.stop="handleClick('wasteland')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('wasteland')" @click.stop="handleClick('wasteland')"
 						:points="coords.wasteland" href="#Wastelands" alt="Wastelands of Pelius" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('shippingYards')" 
-        @click.stop="handleClick('shippingYards')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('shippingYards')" @click.stop="handleClick('shippingYards')"
 						:points="coords.shippingYards" href="#ShippingYards" alt="Siora Polat Shipping Yards" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('solarPlant')" 
-        @click.stop="handleClick('solarPlant')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('solarPlant')" @click.stop="handleClick('solarPlant')"
 						:points="coords.solarPlant" href="#SolarPlant" alt="Siora Power Plant" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('aghoru')" 
-        @click.stop="handleClick('aghoru')"
+      <text class="noselect" x="690" y="1090" text-anchor="middle" fill="white" font-size="30">Traigo</text>
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('aghoru')" @click.stop="handleClick('aghoru')"
 						:points="coords.aghoru" href="#Aghoru" alt="Aghoru Archipelago" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('peridos')" 
-        @click.stop="handleClick('peridos')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('peridos')" @click.stop="handleClick('peridos')"
 						:points="coords.peridos" href="#Peridos" alt="Peridos Gap" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('sancour')" 
-        @click.stop="handleClick('sancour')"
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('sancour')"  @click.stop="handleClick('sancour')"
 						:points="coords.sancour" href="#Sancour" alt="Sancour Industrial Zone" />
-			<polygon shape="poly" class="overlay-zone" :class="zoneColour('gasFields')" 
-        @click.stop="handleClick('gasFields')"
-						:points="coords.gasFields" href="#GasFields" alt="Lavatina Gas Fields" > <span>12</span></polygon>
+			<polygon shape="poly" class="overlay-zone" :class="zoneColour('gasFields')" @click.stop="handleClick('gasFields')" 
+            :points="coords.gasFields"  href="#GasFields" alt="Lavatina Gas Fields" />
+      <div style="position:absolute;left:1000px; top:1000px;" class="zone_text">1</div>
+      <text class="overlay-zone-text noselect" x="157" y="1705" text-anchor="middle" fill="whitesmoke" font-size="20">1</text>
+      <text class="overlay-zone-text noselect" x="580" y="1445" text-anchor="middle" fill="whitesmoke" font-size="20">3</text>
+      <text class="overlay-zone-text noselect" x="510" y="1445" text-anchor="middle" fill="whitesmoke" font-size="20">2</text>
+      <text class="overlay-zone-text noselect" x="593" y="1215" text-anchor="middle" fill="whitesmoke" font-size="20">4</text>
+      <text class="overlay-zone-text noselect" x="630" y="1390" text-anchor="middle" fill="whitesmoke" font-size="20">5</text>
+      <text class="overlay-zone-text noselect" x="780" y="1410" text-anchor="middle" fill="whitesmoke" font-size="20">6</text>
+      <text class="overlay-zone-text noselect" x="718" y="1321" text-anchor="middle" fill="whitesmoke" font-size="20">7</text>
+      <text class="overlay-zone-text noselect" x="1080" y="920" text-anchor="middle" fill="whitesmoke" font-size="20">8</text>
+      <text class="overlay-zone-text noselect" x="1145" y="840" text-anchor="middle" fill="whitesmoke" font-size="20">9</text>
+      <text class="overlay-zone-text noselect" x="1237" y="850" text-anchor="middle" fill="whitesmoke" font-size="20">10</text>
+      <text class="overlay-zone-text noselect" x="1237" y="990" text-anchor="middle" fill="whitesmoke" font-size="20">11</text>
+      <text class="overlay-zone-text noselect" x="1400" y="595" text-anchor="middle" fill="whitesmoke" font-size="20">12</text>
 		</svg>
     <div v-if="displayZone" class="zone_info_box">
       <div class="padding">
@@ -73,11 +85,11 @@
         sectorData: {...sectorData},
         coords: {...coords},
         control: {
-          "orbitalStation": 0,
-          "commsRelay": 0,
-          "ancientRuins": 0,
+          "orbitalStation": 1,
+          "commsRelay": 2,
+          "ancientRuins": 1,
           "hiveCity": 0,
-          "commandComplex": 0,
+          "commandComplex": 3,
           "wasteland": 0,
           "shippingYards": 0,
           "solarPlant": 0,
@@ -87,10 +99,11 @@
           "gasFields": 0
         },
         colours: {
-          0: "red",
-          1: "blue",
-          2: "yellow",
-          3: "green",
+          0: "transparent",
+          1: "red",
+          2: "blue",
+          3: "yellow",
+          4: "green",
         },
         editMode: null,
         displayZone: null
@@ -101,7 +114,7 @@
         this.editMode ? this.rotateControl(zone) : this.showInfo(zone);
       },
       rotateControl(zone) {
-        this.control[zone] <= 2 ? this.control[zone] = this.control[zone] + 1 : this.control[zone] = 0; 
+        this.control[zone] <= 3 ? this.control[zone] = this.control[zone] + 1 : this.control[zone] = 0; 
       },
       showInfo(zone) {
         this.displayZone = sectorData[zone];
@@ -122,15 +135,25 @@
     top: 0;
     left: 0;
     height: 1934px;
+    width: 100%;
+    font-family: "Lucida Console", Courier, monospace;
 
+    .overlay-zone-text {
+      fill: orangered;
+      stroke: orangered;
+      stroke-width: 0.5px;
+      text-anchor: middle;
+      font-size: 25px;
+    }
     .overlay-zone {
-      opacity: 0.5;
-      fill:lime;
+      opacity: 0.3;
+      fill: transparent;
       stroke:black;
       stroke-width:4;
+      cursor: pointer;
 
       &:hover {
-        opacity: 0.8;
+        opacity: 0.6;
       }
 
       &.red {
@@ -161,8 +184,9 @@
     opacity: 0.8;
     border-radius: 30px;
     z-index: 1;
-    font-family: "Lucida Console", Courier, monospace;
     font-size: x-large;
+    font-family: "Lucida Console", Courier, monospace;
+
 
     .padding {
       margin: 20px;
@@ -198,5 +222,62 @@
       padding: 15px 0;
     }
   }
+  .legend {
+    position: absolute;
+    top: 50px;
+    left: 1100px;
+    background-color: white;
+    border-radius: 15px;
+    opacity: 0.8;
+    font-family: "Lucida Console", Courier, monospace;
+    font-size: x-large;
 
+    .title {
+      margin: 10px 0;
+      font-weight: bold;
+      text-align: center;
+    }
+
+    .list {
+      margin: 10px 30px 20px 0px;
+      list-style: none;
+    }
+    
+    li {
+      margin: 5px 0;
+    }
+
+    .legend-box {
+      border: 1px solid #ccc; 
+      float: left; 
+      width: 12px; 
+      height: 12px;  
+      margin: 0 10px;
+
+      &.red {
+        background-color: #ff0000;
+      }
+
+      &.blue {
+        background-color: #0000ff;
+      }
+
+      &.yellow {
+        background-color: #fcf403;
+      }
+
+      &.green {
+        background-color: #008000;
+      }
+    }
+  }
+  .noselect {
+    -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+              user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome and Opera */
+  }
 </style>
--- src/sectorData.js
+++ src/sectorData.js
@@ -14,7 +14,7 @@ export default {
         control: "Its Controller gains the Signal Jam stratagem (1cp): After an opponent uses a stratagem, roll a 1d6, on a 4+ the stratagem fails. The opponent's CP is not refunded."
     },
     "ancientRuins": {
-        name: "Ancient Ruins",
+        name: "Ruined Pillars of the Archius",
         description: "This region is nearly uninhabited nome only to small teams of researchers, covered by lush jungles and strange ancient structures.",
         terrain: "Battles fought here should be fought over lush, green wilderness terrain with large amounts of elevated hills and rocks.",
         effects: "None",
@@ -30,8 +30,8 @@ export default {
         planetControl: "If a player controls the entire planet of Pelius, they may designate one unit at the beginning of each crusade round to gain +5xp."
     },
     "commandComplex": {
-        name: "Command Complex",
-        description: "This sprawling complex dominates the landscape, is designed as a fortress from which to coordinate planitary defences.",
+        name: "The Mountains of Mori",
+        description: "This mountainous landscape is dominated by a spawling fortress complex, from which one can coordinate the planet's defences.",
         terrain: "Battles Fought here should be fought on a field that is rugged, mountainous and wild.",
         effects: "If the Controlling player is fighting in this zone, they gain +10% of their Battle Points Limit as Reinforcements that may only be used to place fortifications.",
         control: "The Controlling player gains +2 Command Points in each battle, but loses 2 Command Points if thier Warlord is slain.",
@@ -40,51 +40,55 @@ export default {
     "wasteland": {
         name: "Wastelands of Pelius",
         description: "This vast desolate region is very inhospitable with only a few outposts capable of harvesting the sparse resources of this region",
-        terrain: "Battle fought here should contain lots of rocks, craters, a few patches of trees or shrubs, and few industrial buildings",
-        effects: "Battle fought here should be fought one the next size of map, and use appropriate mission formats for that map size.",
+        terrain: "Battles fought here should contain lots of rocks, craters, a few patches of trees or shrubs, and few industrial buildings",
+        effects: "Battles fought here should be fought one the next size of map, and use appropriate mission formats for that map size.",
         control: "The Controller of this region gains +1 Requisition Point at the beginning of each Crusade round.",
         planetControl: "If a player controls the entire planet of Pelius, they may designate one unit at the beginning of each crusade round to gain +5xp."
     },
     "shippingYards": {
         name: "Siora Polar Shipping Yards",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        description: "This area is home to the vast low gravity shipping yards that ship all of the goods from the planet below out of the system.",
+        terrain: "Battles fought here should be filled with industrial buildings and barren lunar landscapes.",
+        effects: "None",
+        control: "The Controller of this region gains a major logistics center, which grants +2 Requisition Points at the beginning of each Crusade Round."
     },
     "solarPlant": {
         name: "Siora Power Plant",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        description: "This region is home to a massive power plant that has completely poisoned the area.",
+        terrain: "Battles fought here should be a barren wasteland dominated by a single large industrial structure that is an unstable position, and contain only a few natural features that block line of sight.",
+        effects: "This battlefield is filled with toxic dust and high levels of radiation, Out of action tests must be rolled with 2 dice, discarding the highest after the battle.",
+        control: "At the beginning of the Crusade Round the Controller of this region may supply one of its Infantry units with High Yield Power Packs for the Crusade round; Any energy based weapons (las, plasma, tesla, etc.) have their number of shots characteristic increased by one (eg. Rapid Fire 1, becomes Rapid Fire 2). Cannons, grenades, autoguns and bolters are examples of non energy weapons. Check with your Arbiter if you are unsure."
     },
     "aghoru": {
-        name: "",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        name: "Aghoru Archipelago",
+        description: "Advanced research facilities and Marine ecology stations dot the landscape of this region.",
+        terrain: "Battles Fought here should be a combination of high tech buildings and lush greenery. ",
+        effects: "None",
+        control: "Each Crusade Round this region’s advanced medical facilities allows the Controller to return one of their non-vehicle units that is out of action back to service.",
+        planetControl: "If a player controls the entire planet of Traigo, before the first turn of any battle, they may redeploy 1 unit anywhere in their deployment zone."
     },
     "peridos": {
-        name: "",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        name: "Peridos Gap",
+        description: "This rugged region is rich in minerals and home to large scale mineral exploitation and excavation operations.",
+        terrain: "Battles fought here should be on barren, rocky terrain containing few buildings.",
+        effects: "None",
+        control: "The Controller of this region gains +1 Requisition Points at the beginning of each Crusade Round.",
+        planetControl: "If a player controls the entire planet of Traigo, before the first turn of any battle, they may redeploy 1 unit anywhere in their deployment zone."
     },
     "sancour": {
-        name: "",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        name: "Sancour Industrial Region",
+        description: "This highly industrialized region of the planet is dominated by the planet's Manufactorum.",
+        terrain: "Battles fought here should be in industrial urban terrain.",
+        effects: "None",
+        control: "Each Crusade Round this region's manufactorum provides its controller with +1 Requisition Points, and allows them to use the Rearm and Resupply Requisition once at the beginning of each Crusade Round for free.",
+        planetControl: "If a player controls the entire planet of Traigo, before the first turn of any battle, they may redeploy 1 unit anywhere in their deployment zone."
     },
     "gasFields": {
-        name: "",
-        description: "",
-        terrain: "",
-        effects: "",
-        control: ""
+        name: "Lavantia Gas Fields",
+        description: "This barely habitable region is home to bubbling pools and fields of gases that fuel the vehicles and great ships of the 42nd Millenium.",
+        terrain: "Battles fought here should be on barren wastes, where large natural terrain breaks up line of sight and include a few industrial buildings.",
+        effects: "The drifting clouds of gases that drift across the battlefield make fires and explosions dangerous. Flamers and similar fire based auto-hitting weapons roll twice for the number of hits and take the highest, but any wound rolls of 1 deal 1 mortal sound to the bearer. When rolling to see whether a vehicle or similar unit explodes roll 3 dice instead of one, if any would succeed the vehicle explodes. All random ranges are maximized on this battlefield",
+        control: "The Controller of this region reduces their Command Point Cost of the Strategic Reserves stratagem by 2 to a minimum of 0.",
+        planetControl: "If a player controls the entire planet of Traigo, before the first turn of any battle, they may redeploy 1 unit anywhere in their deployment zone."
     }
 }
